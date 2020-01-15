9529bdc1267817a471ebae681dd6d03624a27e53
Filip Siwiec <filipsiwiec@gmal.com>
2020-01-15 18:37:08 +0100

    instrukcje warunkowe

--- main.js
+++ main.js
@@ -1,95 +1,136 @@
-// typ boolean, wartośc logiczna, typ logiczny
-// tylko dwie wartości tego typu: true i false
+/* Bardzo często potrzebujemy uruchomić różny kod (różny blok kodu) w zaleznośic od stanu (sytuacji, spełnienia warunku) w programie. */
 
-// funkcja Boolean - zmienia wartość innego typu na wartość typu boolean
-Boolean(0) //false
-Boolean(2 >= 2) //true
-Boolean("coś") //true
-Boolean({}) //true
-Boolean(["ktoś", 100, false]) //true
-Boolean("") //false
 
-// Wartości, które są konwertowane na false:
-""; //pusty string, oczywiście też '' i ``
-0; //też -0
-NaN;
-null;
-undefined;
+// INSTRUKCJA WARUNKOWA IF
 
-// OPERATORY ZWRACAJĄCE TYP LOGICZNY
+// Przykład1
+const condition = 2;
 
-//większy/mniejszy porównanie
-2 > 4; // false
-6 >= 5; // true
-2 == 2; //true
-"1" == true; //konwersja na number a potem porównanie
-"1" === true; //false
-"1" === 1; //false
+if (condition) {
+ // alert("działa!");
+ console.log("widzisz mnie ponieważ warunek jest prawdziwy i wykonuje zawartość bloku instrukcji warunkowej");
+}
 
-// Negacja logiczna - zamień na typ logiczny (jesli nie jest) i odwróc
-!false; //true
-!0; //true
-!200; //false
-!true; //false
 
-// podwójna negacja
-!!true
-!!0
-!!""
+// Kod w bloku jest uruchomiony TYLKO WTEDY gdy warunek jest prawdziwy. Jeśli nie jest prawdziwy, to blok jest pomijany ({} - pomijany)
 
-const userName1 = "Ścibor";
-const userName2 = "";
-!!userName1
-!!userName2
+// Przykład 2
+const userName = "Dobromir";
 
-// LOGICZNE "I" (iloczyn logiczny) &&
-//oba elementy prawdziwe
-let userAge = 22;
-let userMoney = 10.2;
-let beerPrice = 11;
-// warunek
-(userAge >= 18 && userMoney > beerPrice)
+if (userName) {
+ console.log("Witaj " + userName + "!")
+}
+// to samo, co wyżej z innaczej skonstruowanym warunkiem jednak oznaczajacym to samo.
 
-// console.log(userAge > 18 && userMoney > beerPrice);
+if (userName !== "") {
+ console.log("Witaj " + userName)
+}
 
-//Jak to działa
+// INSTRUKCJA WARUNKOWA ELSE (IF...ELSE)
+const hasTicket = true;
+const moreThen15Years = true;
+const bossFamily = false;
 
-1 && 0;
-/*
-1- Sprawdza lewą stronę, dokonując konwersji wartości (wyrażenia) na typ boolean. Jeśli jest true to sprawdza drugą stronę, jeśli nie to zwraca tę wartość. np. "" && 1 - kończy działanie po pierwszym i zwraca "" (psty string) 
+if (hasTicket && moreThen15Years || bossFamily) {
+ console.log("wchodzisz");
+} else {
+ console.log("przykro mi");
+}
 
-2 - Po przejściu do prawej strony sprawdza ją i zwraca ją.
+// else wykonuje się tylko w wypadku gdy warunek if nie jest prawdą. Jeśli jest prawdą to else jest pomijany.
 
-Całe wyrażenie możemy potraktować jako prawdziwe tylko jeśli obie strony są prawdziwe (są true lub po konwersji na boolean będą true)
-false && false  //false
-true && false // false
-false && true // false
-true && true // true
+// KLAUZULA ELSE IF
+const age = "20";
+
+// Tak nie powinniśmy - bo to są osobne instrukcje sprawdzane za każdym razem oddzielnie.
+
+if (age <= 9) {
+ console.log("jesteś jeszcze dzieckiem")
+}
+if (age > 9 && age <= 18) {
+ console.log("jesteś młodzieżą")
+}
+if (age > 18 && age <= 70) {
+ console.log("cały świat przed Tobą")
+}
+if (age > 70) {
+ console.log("Jesteś dojrzałem człowiekiem i doceniasz zalety tego wieku")
+}
+
+// Trudniej pomylić warunek i zdecydowanie lepiej zoptymalizowane
+
+if (age <= 9 && typeof age === "number") {
+ console.log("jesteś jeszcze dzieckiem")
+} else if (age <= 18 && typeof age === "number") {
+ console.log("jesteś młodzieżą")
+} else if (age <= 70 && typeof age === "number") {
+ console.log("cały świat przed Tobą")
+} else {
+ console.log("Jesteś dojrzałem człowiekiem i doceniasz zalety tego wieku lub nie mogę ustalić twoejgo wieku")
+}
+
+
+// ZAGNIEŻDZENIE IF
+const guestName = "Bolesław";
+const guestAge = 30;
+
+if (guestName != "") {
+ if (guestAge > 18) {
+  console.log("Witaj w klubie " + guestName);
+ }
+ else {
+  console.log("No niestety " + guestName + " nie wjedziesz")
+ }
+}
+else {
+ console.log("nie wiem jak masz na imię, więc nie wejdziesz");
+}
+
+// INSTRUKCJA WARUNKOWA SWITCH
+//Czasami bardziej przejrzysta niż instrukcja if bedzie instrukcja switch, która tworzy drzewko z możliwymi opcjami. 
+
+const dayOfTheWeek = "środa"
+
+// jako argument podajemy wartość, którą będziemy porównywać.
+switch (dayOfTheWeek) {
+ //po case podajemy wartość do porównania z argumentem instrukcji
+ case "poniedziałek":
+  console.log("dziś poniedziałek");
+  break;
+ case "wtorek":
+  console.log("dziś wtorek");
+  break;
+ case "środa":
+  console.log("dziś środa");
+  break;
+ default:
+  console.log("nie ma pojęcia co to za dzień");
+  break;
+}
+
+// OPERATOR WARUNKOWY / OPERATOR POTRÓJNY (TRÓJKOWY)
+
+// Prostą instrukcję warunkąwą if...else możemy napisać za pomocą operatora.
+
+/* 
+a ? b : c;
+warunek ? wykonaj mnie jesli warunek true : wykonaj mnie jeśli warunek false
 */
+const score = 100
+20 == "20" && score ? console.log("wykonuje się, bo prawda") : console.log("wykonuje się bo warunek nie jest prawdziwy");
 
-// LOGICZNE "LUB" (suma logiczna, alternatywa) ||
+score > 120 ? console.log("prawda") : console.log("fałsz");
 
-const userConnections = "syn głównej księgowej";
-const userExperience = 0;
+// Z instrukcji warunkowej nie można przechwycić wartości i przypisać jej do zmiennej. A w pewnych sytuacjach chcemy to zrobić.
+// Operator warunkowy zwraca wartość (jako to operatory) i jest używany często gdy do zmiennej chcemy przypisać wynik działania
 
-// Tylko jeden z operandów musi być true (czy konwertowany na true)
-Boolean(userConnections || userExperience)
+// Przykład zwracania wartości - instrukcji warunkowej if tak nie użyjemy
+// console.log(1 > 10 ? 1 : 0)
 
-1 || 0;
-/*
-1- Sprawdza lewą stronę, dokonując konwersji wartości (wyrażenia) na typ boolean. Jeśli jest true to NIE SPRAWDZA już drugiej strony i zwraca wartość, którą sprawdzał (tak byłoby w przykładzie 1 || 0). Warunek na tym etapie jest już rpawdziwy. Jeśli jest false, to przechodzi do sprawdzenie argumentu z prawej strony np. "" || 0;
-
-2 - Po przejściu do prawej strony sprawdza ją (konwertuje na typ boolean jeśli nie jest) i zwraca ją niezależnie jaka będzie. Całe wyrażeniej jest prawdziwe jeśli choć jedna strona jest true (mogą być też obie)
-
-false || false  //false
-true || false // true
-false || true // true
-true || true // true
-*/
+let gameResult = 0;
+let killedMonsters = 9
+let score = killedMonsters > 10 ? 1 : 0;
 
 
-// Konwersja w drugą stronę - z boolean na inny typ (number i string)
-Number(true); //1
-Number(false); //0
-String(true); // "true"
-String(false); // false
+const playerName = "Mściowej";
+console.log(`Witaj ${playerName ? playerName : "nieznajomy wojowniku"}`)
