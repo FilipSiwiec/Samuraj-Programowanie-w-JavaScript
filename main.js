fb2d16422ac121e1f8c52abb2f25e9a644e79d20
Filip Siwiec <filipsiwiec@gmal.com>
2020-01-22 11:42:15 +0100

    hoisting

--- main.js
+++ main.js
@@ -1,64 +1,66 @@
 
-// Operator rest (ES6) - wiele elementów podanych jako argumenty do jednej tablicy
-//...nazwa (dowolna zgodnie z zasadami dla zmiennej)
-// zwraca tablicę (arguments nie jest tablicą)
+// CO TO JEST HOISTING - CO DZIEJE SIĘ PO WYWOŁANIU KODU
 
-function showAllArguments(...items) {
-    console.log(items);
-    // console.log(arguments[5]);
-    return items;
-   }
-   // showAllArguments("1", 2, 12, "ab", true, "Adam")
-   
-   function addAllWords(...words) {
-    let txt = "";
-    // for (let i = 0; i < words.length; i++) {
-    //  // txt += words[i] + "-";
-    //  txt += `${words[i]}-`;
-    // }
-   
-    // words.forEach(function(word) {
-    //  // txt += word + "-";
-    //  txt += `${word}-`
-    // })
-   
-    words.forEach(word => txt += `${word}-`)
-   
-    console.log(txt);
-   }
-   // addAllWords()
-   // addAllWords("ja", "ty", 2, "Gdańsk")
-   
-   // dzięki operatorowi rest możemy zbierać pozostałe argumenty (które zostaly umieszczone poza paramterami)
-   
-   function showUsers(owner, ...others) {
-    console.log(`Na imprezie był ${owner} ${others.length ? "oraz " + others + "." : "."}`)
-   }
-   
-   // showUsers("Adrian");
-   // showUsers("Adrian", "Jadzia", "Staszek", "Henio");
-   
-   
-   /* ---------------------------------------- */
-   
-   // METODY A FUNKCJE
-   // Metody to funkcje umieszczone w obiektach (poznamy w przyszłości). 
-   
-   const objectExample = {
-    name: "Adam",
-    showName: function() {
-     console.log("Jan")
-    },
-    showAge() {
-     console.log(30);
-    }
-   }
-   
-   // objectExample.showName();
-   // objectExample.showAge();
-   
-   const showName = function() {
-    console.log("Jan")
-   }
-   
-   // showName();
+// Interpreter czyta nasz kod od początku do końca, więc kolejność ma znaczenie, ale hoisting ten proces zaburza.
+
+// Zanim kod zostanie wykonany jest przetworzony. Interpreter przechodzi przez kod kilka razy (szuka błędów, wyszukuje deklaracji...).
+
+// Hoisting - przeniesienie części deklaracji na początek zakresu.
+
+// ZMIENNE
+
+// console.log(typeNumber);
+// console.log(typeString);
+// console.log(typeBoolean);
+// console.log(tablica);
+// console.log(obiekt);
+// console.log(funkcja);
+
+// dwie instrukcje: deklaracja i inicjalizacja (przypisanie wartości)
+const typeNumber = 20;
+let typeString = "tekst";
+var typeBoolean = false;
+
+var tablica = [];
+let obiekt = {};
+const funkcja = function() {
+};
+
+
+
+// Wartośc przypisana do zmiennej nie ma znaczenie. Liczy się sposób deklracji (const, let, var). Jeśli stworzyliśmy zmienną za pomocą var i próbujemy jej użyć przed tym za nim ją zadeklraowaliśmy to one będzie istniej, ale będzie miała przypisaną wartość undefined. Hoistingowi pzy var podlega tylko deklaracja.
+
+//Jeśli zadeklarowaliśmy zmienną za pomocą const lub let i próbujemy jej użyć wcześniej, to nawet nie będzie undefined tylko błąd. Zmienna "is not defined". Ponieważ const i var nie podlega hoistingowi.
+
+/* --------------------------------- */
+
+
+// FUNKCJA STWORZONA ZA POMOCĄ DEKLARACJI
+// Funkcja stworzona za pomocą deklaracji (a nie przypisania do zmiennej) podelega hoistingowi i może być wywołana nawet przed jej zadeklarowaniem.
+
+// funkcjaNazwana();
+
+// console.log(funkcjaLet);
+// console.log(funkcjaVar);
+
+function funkcjaNazwana() {
+ console.log("jestem");
+}
+
+// funkcjaConst() //błąd - nie jest zdefiniowe, bo zmienna const nie podelga hoistingowi
+
+console.log(funkcjaVar)
+// funkcjaVar() // na tym etapie (przed stworzeniem) będzie zawierała undefined więc tak naprawdę próbujemy tu zrobić coś takiego undefined()
+
+
+const funkcjaConst = function() {
+ console.log("ok");
+};
+let funkcjaLet = function() { };
+
+var funkcjaVar = function() { };
+
+
+// Reasumująć hositingowi (przenoszeniu deklaracji na początek zakresu) podlegają funkcje i deklaracje zmiennych, ale tylko za pomocą var. Hoistingowi nie podlega przypisane wartości w zmiennych.
+
+// Trzeba wiedzieć, ze hoisting istnieje, nie koniecznei trzeba to "wykorzystywać". Być może najlepiej go unikać (a więc nie uzywać var i deklaracji funkcji)
