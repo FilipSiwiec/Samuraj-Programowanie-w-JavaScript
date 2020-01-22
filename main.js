1022ab18d33360e465201ba8dfd03ad02e82dd78
Filip Siwiec <filipsiwiec@gmal.com>
2020-01-22 13:09:10 +0100

    scope

--- main.js
+++ main.js
@@ -1,66 +1,43 @@
+/* CO TO JEST ZAKRES (SCOPE) - WPROWADZENIE */
 
-// CO TO JEST HOISTING - CO DZIEJE SIĘ PO WYWOŁANIU KODU
+// Zakres mówi gdzie dana zmienna jest widoczna i gdzie może być wykorzystana.
 
-// Interpreter czyta nasz kod od początku do końca, więc kolejność ma znaczenie, ale hoisting ten proces zaburza.
+// Zakres globalny (główny) i zakres lokalny (zakres lokalny tworzy blok lub ciało funkcji).
 
-// Zanim kod zostanie wykonany jest przetworzony. Interpreter przechodzi przez kod kilka razy (szuka błędów, wyszukuje deklaracji...).
+// W naszym programie (złożonym) istnieje mnóstwo zakresów. Każdy blok, funkcja, towrzy swój zakres. Jeden zakres może być zagnieżdzony w innym zakresie. 
 
-// Hoisting - przeniesienie części deklaracji na początek zakresu.
-
-// ZMIENNE
-
-// console.log(typeNumber);
-// console.log(typeString);
-// console.log(typeBoolean);
-// console.log(tablica);
-// console.log(obiekt);
-// console.log(funkcja);
-
-// dwie instrukcje: deklaracja i inicjalizacja (przypisanie wartości)
-const typeNumber = 20;
-let typeString = "tekst";
-var typeBoolean = false;
-
-var tablica = [];
-let obiekt = {};
-const funkcja = function() {
-};
+//Zakres jest zależny od tego gdzie dana zmienna znajduje się w kodzie (gdzie została utworzona).
 
 
+/* ZAKRES GLOBALNY */
+// Poza blokami i funkcjami. Przestrzeń najwyższego rzędu.
 
-// Wartośc przypisana do zmiennej nie ma znaczenie. Liczy się sposób deklracji (const, let, var). Jeśli stworzyliśmy zmienną za pomocą var i próbujemy jej użyć przed tym za nim ją zadeklraowaliśmy to one będzie istniej, ale będzie miała przypisaną wartość undefined. Hoistingowi pzy var podlega tylko deklaracja.
+// var userID = 102;
+// const colorID = 5;
+// let userAge = 20;
 
-//Jeśli zadeklarowaliśmy zmienną za pomocą const lub let i próbujemy jej użyć wcześniej, to nawet nie będzie undefined tylko błąd. Zmienna "is not defined". Ponieważ const i var nie podlega hoistingowi.
+// console.log(userID);
+// console.log(colorID);
+// console.log(userAge);
 
-/* --------------------------------- */
+// userID = 200;
+// colorID = 10;
+// userAge = 21;
 
 
-// FUNKCJA STWORZONA ZA POMOCĄ DEKLARACJI
-// Funkcja stworzona za pomocą deklaracji (a nie przypisania do zmiennej) podelega hoistingowi i może być wywołana nawet przed jej zadeklarowaniem.
+// console.log(userID);
+// console.log(colorID);
+// console.log(userAge);
 
-// funkcjaNazwana();
+// Zmienne zadeklarowane w zakresie globalnym są widoczne wszędzie w programie (również w innych zakresach)
 
-// console.log(funkcjaLet);
-// console.log(funkcjaVar);
+let userName = "Adam";
 
-function funkcjaNazwana() {
- console.log("jestem");
+function showName() {
+ let userName = "Jan";
+ userName = "Marysia";
+ console.log(userName);
 }
 
-// funkcjaConst() //błąd - nie jest zdefiniowe, bo zmienna const nie podelga hoistingowi
-
-console.log(funkcjaVar)
-// funkcjaVar() // na tym etapie (przed stworzeniem) będzie zawierała undefined więc tak naprawdę próbujemy tu zrobić coś takiego undefined()
-
-
-const funkcjaConst = function() {
- console.log("ok");
-};
-let funkcjaLet = function() { };
-
-var funkcjaVar = function() { };
-
-
-// Reasumująć hositingowi (przenoszeniu deklaracji na początek zakresu) podlegają funkcje i deklaracje zmiennych, ale tylko za pomocą var. Hoistingowi nie podlega przypisane wartości w zmiennych.
-
-// Trzeba wiedzieć, ze hoisting istnieje, nie koniecznei trzeba to "wykorzystywać". Być może najlepiej go unikać (a więc nie uzywać var i deklaracji funkcji)
+// showName()
+// console.log(userName);
