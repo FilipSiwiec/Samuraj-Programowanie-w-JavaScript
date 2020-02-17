76db1bf7651898f907d6cf6200af203134699dd7
Filip Siwiec <filipsiwiec@gmal.com>
2020-02-17 22:03:59 +0100

    Projekt 4 - 1c

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 let p = document.querySelector("p");
 
 document.body.addEventListener("mousemove", (e) => {
-    console.log("dziala");
+    // console.log("dziala");
+    const x = e.clientX;
+    const y = e.clientY;
 
-    p.textContent = `${e.clientX}, ${e.clientY}`;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-    document.body.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY} ,100)`;
+
+    const red = x / width * 100;
+    const green = y / height * 100; 
+    const blue =  ((x / width * 100) + (y / height * 100)) / 2;
+
+
+    p.textContent = `${x}, ${y}`;
+
+    document.body.style.backgroundColor = `rgb(${red}%, ${green}% ,${blue}%)`;
 })
