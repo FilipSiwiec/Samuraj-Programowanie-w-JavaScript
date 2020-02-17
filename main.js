22b3690662a19601d2ccba0c2a75f559cd3c4baa
Filip Siwiec <filipsiwiec@gmal.com>
2020-02-17 18:36:46 +0100

    Projekt4 - 1a

--- main.js
+++ main.js
@@ -1,43 +1,9 @@
-let size = 10;
-let orderElement = 1;
+let p = document.querySelector("p");
+document.body.addEventListener("mousemove", (e => {
+// console.log(event.clientX, event.clientY)
 
-const init = () => {
- // tutaj kod
- console.log('dziala');
- const guzik = document.createElement("button");
+p.textContent = `${e.clientX}, ${e.clientY}`;
+// p.textContent = e.clientX + "," + e.clientY ;
+// p.textContent = `${e.screenX}, ${e.screenY}`;
 
- guzik.style.background = 'red';
- guzik.style.width = 100 + "px";
- guzik.style.height = 50 + "px";
- guzik.textContent = "Dodaj 10 elementów listy";
- document.body.appendChild(guzik);  
-
- const liczby = document.createElement("ul");
- document.body.appendChild(liczby);  
-
- guzik.addEventListener("click", createLiElements);
-
- 
-
-}
-
-const createLiElements = () => {
- 
- 
- console.log("tez dziala");
-
- for ( let i = 1; i < 11; i++ ){
-    
-
-    const liczba = document.createElement('li');
-    liczba.textContent = "Element nr " + orderElement;
-    orderElement++;
-    liczba.style. listStyleType= "none";
-    document.querySelector("ul").appendChild(liczba);
-    liczba.style.fontSize = size + "px";
-    size++;
-   }
-
- }
-
-init()
+}))
