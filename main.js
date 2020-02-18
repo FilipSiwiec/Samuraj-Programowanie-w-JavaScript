f7f681e038c5f68e51e43555f52c7fa95a2a964d
Filip Siwiec <filipsiwiec@gmal.com>
2020-02-18 11:57:07 +0100

    Projekt 4 - task 4 start

--- main.js
+++ main.js
@@ -1,45 +1,21 @@
-const div = document.querySelector("div");
+/* Gotowy projekt - sprzawdz działanie strzałek i zrób podobnie. http://websamuraj.pl/examples/js/projekt4-zad2/ */
 
-let divX = 150;
-let divY = 100;
+let red = 100;
+let green = 100;
+let blue = 100;
 
-div.style.left = `${divX}px`;
-div.style.top = `${divY}px`;
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
-let akcja = false;
+const changeColor = (e) => {
+  console.log(e.keyCode, e.which);
+  // 38 - strzałka do góry
+  // 40 - strzalka w dół
 
-let insertDivX;
-let insertDivY;
+  // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
+  //tutaj twój kod
+  // wersja 1 - instrukcja if
+  // wersja 2 - instrukcja switch
 
-div.addEventListener("mousedown", (e) => {
-  // console.log("dole");
-  div.style.backgroundColor = "grey";
-  
-  akcja = !akcja;
-
-  insertDivX = e.offsetX;
-  insertDivY = e.offsetY;
-  console.log(insertDivX,insertDivY);
-
-});
-
-div.addEventListener("mousemove", (e) => {
-  // console.log("rusza");
-
-  if(akcja){
-
-  div.style.left = `${divX}px`;
-  div.style.top = `${divY}px`;
-
-  divX = e.clientX - insertDivX;
-  divY = e.clientY - insertDivY;
 }
 
-});
-
-div.addEventListener("mouseup", () => {
-  // console.log("gora")
-  div.style.backgroundColor = "black";
-
-  akcja = false;
-})
+window.addEventListener('keydown', changeColor)
