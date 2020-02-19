df1511e08aed1ceb98ecdc7c9a4566dc79ee7d76
Filip Siwiec <filipsiwiec@gmal.com>
2020-02-19 20:48:25 +0100

    Projekt 6 - task 4

--- main.js
+++ main.js
@@ -1,24 +1,63 @@
-const names = [];
-const div = document.querySelector("div");
+let tablica = ["walcz", " przemyś to jeszcze raz"];
+const input = document.querySelector("input")
+const h1 = document.querySelector("h1");
 
-const addName = (e) => {
-        e.preventDefault();
-        const input = document.querySelector("input");
-        const newName = input.value;
+// DODAJ 
+const dodaj = document.querySelector(".add");
 
-    if ( newName.length > 0 ) {
-        for ( name of names ) {
-            if ( name === newName ) {
-                return;
-            }
+const dodajMozliwosci = (e) => {
+    e.preventDefault();
+    const newName = " " + input.value;
+
+    for (name of tablica) {
+        if (name === newName) {
+         alert("to już jest!!!!!!");
+         return
         }
-        names.push(newName);
-        console.log(names);
-        div.textContent += newName +", "; 
-        input.value = "";
-    }
+       }
+
+    // let tablica = ["walcz", "przemyś to jeszcze raz"];
     
+    tablica.push(newName);
+    console.log (tablica);
+    // console.log (input.value);
+    input.value = "";
 }
 
-document.querySelector("button").addEventListener("click", addName);
+dodaj.addEventListener("click", dodajMozliwosci);
 
+// RESET 
+const reset = document.querySelector(".clean");
+
+const resetArray = () => {
+    
+    tablica = [];
+    console.log(tablica);
+}
+
+reset.addEventListener("click", resetArray);
+
+// Przycisk pokaż odpowiedź
+const odp = document.querySelector(".showAdvice");
+
+const odpFunction = () => {
+    // console.log("dziala");
+
+    const index = Math.floor(Math.random() * tablica.length);
+
+    const odpowiedz = Math.random(tablica);
+    console.log(Math.random(tablica))
+    h1.textContent = tablica[index];
+}
+
+odp.addEventListener("click", odpFunction);
+
+// alert
+const alerT = document.querySelector(".showOptions");
+
+const alertFunction = () =>{
+    console.log ("dziala");
+    alert(tablica);
+}
+
+alerT.addEventListener("click", alertFunction);
