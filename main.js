b371430f1625eccf1e905a72ba44818185b5b61c
Filip Siwiec <filipsiwiec@gmal.com>
2020-02-19 23:40:27 +0100

    Projekt 6 - Bonus

--- main.js
+++ main.js
@@ -1,63 +1,29 @@
-let tablica = ["walcz", " przemyś to jeszcze raz"];
-const input = document.querySelector("input")
-const h1 = document.querySelector("h1");
+// Nie zawsze chcemy liczbę losowaną od zera do ...
+//Czasami chcemy od do gdzie pierwsza wartość nie jest zerem
 
-// DODAJ 
-const dodaj = document.querySelector(".add");
 
-const dodajMozliwosci = (e) => {
-    e.preventDefault();
-    const newName = " " + input.value;
+Math.random() * 7 // od 0 do 7 (bez 7)
 
-    for (name of tablica) {
-        if (name === newName) {
-         alert("to już jest!!!!!!");
-         return
-        }
-       }
+// od do
+//od 4 do 6 (bez 6) - > np. 4.3246453, 5.6755434
+// x >=4 i x < 6
+Math.random() * (6 - 4) + 4;
 
-    // let tablica = ["walcz", "przemyś to jeszcze raz"];
-    
-    tablica.push(newName);
-    console.log (tablica);
-    // console.log (input.value);
-    input.value = "";
+// Funkcja uniwersalna
+const numberRandom = (min, max) => {
+ const number = Math.random() * (max - min) + min
+ return number;
 }
 
-dodaj.addEventListener("click", dodajMozliwosci);
+// od do (liczba całkowita)
+// od 4 do 6 (wliczając) - > 4,5,6 
+// x >=4 i x <= 6
 
-// RESET 
-const reset = document.querySelector(".clean");
+Math.floor(Math.random() * (6 - 4 + 1) + 4);
 
-const resetArray = () => {
-    
-    tablica = [];
-    console.log(tablica);
-}
+// const numberRandomInt = (min, max) => {
+//  const number = Math.floor(Math.random() * (max - min + 1) + min);
+//  return number;
+// }
 
-reset.addEventListener("click", resetArray);
-
-// Przycisk pokaż odpowiedź
-const odp = document.querySelector(".showAdvice");
-
-const odpFunction = () => {
-    // console.log("dziala");
-
-    const index = Math.floor(Math.random() * tablica.length);
-
-    const odpowiedz = Math.random(tablica);
-    console.log(Math.random(tablica))
-    h1.textContent = tablica[index];
-}
-
-odp.addEventListener("click", odpFunction);
-
-// alert
-const alerT = document.querySelector(".showOptions");
-
-const alertFunction = () =>{
-    console.log ("dziala");
-    alert(tablica);
-}
-
-alerT.addEventListener("click", alertFunction);
+const numberRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
