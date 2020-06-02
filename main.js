9235757ff161796cf2748b3b1d02d28e1e22ad3e
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-02 13:44:10 +0200

    Prohjekt 7 - part 5

--- main.js
+++ main.js
@@ -29,10 +29,12 @@ const gameSummary = {
    //  console.log(e.currentTarget);
    // }
    
+   // Funkcja określająca wybór komputera
    function aiChoice() {
     return hands[Math.floor(Math.random() * 3)].dataset.option;
    }
    
+   // Funkcja zwracajająca informacje o wyniku gry
    function checkResult(player, ai) {
     // console.log(player, ai);
     if (player === ai) {
@@ -42,6 +44,29 @@ const gameSummary = {
     } else { return 'loss'; }
    }
    
+   // Publikacja wyniku
+   
+   function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+   
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+   
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+   
+    if (result === "win") {
+     document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+     document.querySelector('[data-summary="who-win"]').textContent = "Ty wygrałeś!!!!"
+     document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+     document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+     document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał :("
+     document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+     document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+     document.querySelector('[data-summary="who-win"]').textContent = "Remis :\\"
+     document.querySelector('[data-summary="who-win"]').style.color = "gray";
+    }
+   }
    //funkcja sterująca
    function startGame() {
     if (!game.playerHand) {
@@ -50,6 +75,7 @@ const gameSummary = {
     game.aiHand = aiChoice();
     const gameResult = checkResult(game.playerHand, game.aiHand);
     console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult)
    }
    
    hands.forEach(hand => hand.addEventListener('click', handSelection))
