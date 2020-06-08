ac5654fc011786b8022cf1198888db9f0d13e6e6
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-08 16:01:55 +0200

    section 10 - projekt 8 - task 1b

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const text = document.querySelector(".text");
-const Lorem = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, est? Recusandae, nobis? Reprehenderit facilis saepe eius a minus assumenda, officiis recusandae culpa ipsum sint perferendis numquam doloribus repudiandae veniam. Temporibus?";
+const pionowa = document.querySelector(".kursor");
+const Lorem = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, est? Recusandae, nobis? Reprehenderit facilis saepe eius a minus assumenda, officiis recusandae culpa ipsum sint perferendis numquam doloribus repudiandae veniam. Temporibus? KONIEC";
 
 const time = 40;
 let number = 0;
@@ -7,7 +8,13 @@ let number = 0;
 const funckcjaNasza = () =>{
     text.textContent += Lorem[number]
     number++;
-    if (number == 239) clearInterval(waly);
+    if (number === Lorem.length) clearInterval(waly);
 }
 
 const waly = setInterval(funckcjaNasza, time);
+
+const klawisz = () =>{
+    pionowa.classList.toggle("active");
+}
+
+setInterval(klawisz, 400)
