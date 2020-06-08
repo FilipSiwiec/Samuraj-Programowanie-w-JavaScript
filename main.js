6963a1de8ee1ac784ee3ddb182f542fb490d6880
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-08 22:45:29 +0200

    section 11 - project 9 - task1

--- main.js
+++ main.js
@@ -1,47 +1,27 @@
-// http://websamuraj.pl/examples/js/projekt8/
-// Uzyskaj efekt jak pod tym linkiem 
-// Użyj setTimeout zamiast setInterval,
-// Opóźniej start pisanie każdego słowa
-// Uwaga: Zadanie bardzo trudne
+const slider = [{
+    img: "images/img1.jpg",
+    text: "pierwszy tekst",
+},
+{
+    img: "images/img2.jpg",
+    text: "drugi tekst",
+},
+{
+    img: "images/img3.jpg",
+    text: "trzeci tekst", 
+}]
 
-const spnText = document.querySelector('.text');
-const spnCursor = document.querySelector('.cursor');
-const txt = ['tekst1', 'tekst2', 'tekst3']
+let time = 3000;
+let active = 0;
 
-let activeLetter = -15;
-let activeText = 0;
+const img = document.querySelector("img.slider");
+const h1 = document.querySelector("h1.slider");
 
-
-// Implementacja
-const addLetter = () => {
- // Użyj w środku setTimeout
- if (activeLetter >= 0) {
-  spnText.textContent += txt[activeText][activeLetter];
- }
- activeLetter++;
- if (activeLetter === txt[activeText].length) {
-
-  activeText++;
-  if (activeText === txt.length) return;
-
-  return setTimeout(() => {
-   activeLetter = -15;
-   spnText.textContent = '';
-   addLetter();
-  }, 2000)
-
-
- }
- setTimeout(addLetter, 100)
-
+const funkcja = () => {
+    active++;
+    if (active === slider.length) {active = 0};
+    img.src = slider[active].img;
+    h1.textContent = slider[active].text;
 }
 
-
-addLetter(); //pierwsze wywołanie
-
-
-// Animacja kursora (zostaw bez zmian)
-const cursorAnimation = () => {
- spnCursor.classList.toggle('active');
-}
-setInterval(cursorAnimation, 400);
+setInterval(funkcja, time)
