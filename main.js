2be4deccf34be4e7544f5025ba57052a432d1ac8
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-08 15:47:01 +0200

    section 10 / project 8 - a1

--- main.js
+++ main.js
@@ -1,61 +1,13 @@
-const gamesChoices = {
-    handChoice: "",
-    aiChoice: "",
+const text = document.querySelector(".text");
+const Lorem = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, est? Recusandae, nobis? Reprehenderit facilis saepe eius a minus assumenda, officiis recusandae culpa ipsum sint perferendis numquam doloribus repudiandae veniam. Temporibus?";
+
+const time = 40;
+let number = 0;
+
+const funckcjaNasza = () =>{
+    text.textContent += Lorem[number]
+    number++;
+    if (number == 239) clearInterval(waly);
 }
 
-const stats = {
-    gameNumbers: 0,
-    winings: 0,
-    losses: 0,
-    draws: 0, 
-}
-
-let czlowiek = gamesChoices.handChoice;
-let komputer = gamesChoices.aiChoice;
-
-const hands = [...document.querySelectorAll(".select img")];
-
-hands.forEach( hand => hand.addEventListener("click", 
-function(){
-    hands.forEach(hand => hand.style.boxShadow = "");
-    this.style.boxShadow = '0 0 0 4px red';
-    czlowiek = this.dataset.option;
-}));
-
-function guzik (){
-    if (!czlowiek) return alert("Wybierz opcję!!!");
-    else if( czlowiek ){
-        document.querySelector('[data-summary="your-choice"]').textContent = czlowiek;
-        komputer = hands[Math.floor(Math.random()*3)].dataset.option;
-        document.querySelector('[data-summary="ai-choice"]').textContent = komputer;
-
-        if(czlowiek === komputer){
-            document.querySelector('[data-summary="who-win"]').textContent = "remis!";
-            document.querySelector('[data-summary="who-win"]').style.color = "grey";
-
-            document.querySelector(".draws span").textContent = ++stats.draws;
-        }
-        else if((czlowiek === "papier" && komputer === "kamień") || (czlowiek === "kamień" && komputer === "nożyczki") || (czlowiek === "nożyczki" && komputer === "papier")){
-            document.querySelector('[data-summary="who-win"]').textContent = "wygrana!";
-            document.querySelector('[data-summary="who-win"]').style.color = "red";
-
-            document.querySelector(".wins span").textContent = ++stats.winings;
-        }
-        else{
-            document.querySelector('[data-summary="who-win"]').textContent = "przegrana...";
-            document.querySelector('[data-summary="who-win"]').style.color = "green";
-
-            document.querySelector(".losses span").textContent = ++stats.losses;
-        }
-
-        document.querySelector(".numbers span").textContent = ++stats.gameNumbers;
-        
-    }
-
-    hands.forEach(hand => hand.style.boxShadow = "");
-    czlowiek = "";
-
-}
-
-document.querySelector(".start").addEventListener("click", guzik)
-
+const waly = setInterval(funckcjaNasza, time);
