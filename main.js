d37a6f8043ea9380f6f3349dbd118659ca925231
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-17 19:47:09 +0200

    section 13 - project 11 - task 1

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 
-
-const funkcja = () => {
-    let number = 0;
-    const funkcja2 = () => {
-        console.log(number);
-        document.body.textContent = `${number} sekund`;
-        number++;
-        setTimeout(funkcja2, 1000);
-    }
-
-     funkcja2()
-}
-
-funkcja ();
+const clock = () => {
+    const time = new Date();
+    // console.log(time.toLocaleString());
+    // console.log(time.toLocaleDateString());
+    const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+   
+    document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
+   }
+   
+   setInterval(clock, 1000);
+   
