f09be12df0a67be7c3da42c5e5ac17d7ac13bb84
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-19 13:21:14 +0200

    section 14 - project 12 - task 1a

--- main.js
+++ main.js
@@ -1,38 +1,5 @@
-const main = document.querySelector(".main");
-const reset = document.querySelector(".reset");
-const div = document.querySelector(".time div");
-
-let active = false;
-let number = 0;
-let Int;
-
-const start = () => {
-
-    if (!active) {
-        active = !active;
-        main.textContent = "Pauza";
-        Int = setInterval(odliczanie, 10);
-
-
-    }
-    else {
-        active = !active;
-        main.textContent = "Start";
-        clearInterval(Int);
-    }
+const funkcja = (e) => {
+    e.target.remove();
 }
 
-const resetowanie = () => {
-    console.log("reset");
-    number = 0;
-    div.textContent ="---";
-}
-
-odliczanie = () => {
-    number++;
-    div.textContent = (number / 100).toFixed(2);
-    
-};
-
-main.addEventListener("click", start);
-reset.addEventListener("click", resetowanie);
+document.querySelectorAll("li").forEach( item => item.addEventListener("click", funkcja));
