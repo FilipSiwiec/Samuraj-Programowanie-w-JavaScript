84cd9b91c5010042c6a78ed04e3546dd640951c7
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-23 21:20:34 +0200

    section 15 - OOP - klasy

--- main.js
+++ main.js
@@ -1,93 +1,63 @@
-/* ----- Czym jest klasa W JS ----- */
-// Klasa jest tym czymś czego potrzebował JS :). Naprawdę ułatwia życie programisty. Dostępna od ES6
-// Klasa jest schematem (wzorem) na podstawie którego tworzymy nowe obiekty, instancje, egzemplarze klasy. 
-// Klasa pozwala stosować w obiekcie dziedziczenie, hermetyzacje, polimorfizm 
-// Klasa jest lukrem składniowym, pozwala napisać łatwiej, ładniej, szybciej, ale ciągle to jest to samo co już mogliśmy zrobić przed ES6, zanim pojawiły się klasy (np. za pomocą konstruktora funkcji).
+/* --- KLASY I DZIEDZICZENIE --- */
 
-/* ----- Jak tworzymy klasę ----- */
-// słowo kluczowe class
-// w środku klasy metody (żadna metoda nie jest obowiązkowa)
-// to co umeiszczamyu w konstruktorze jest potem umeiszczane (jako kopia) w każdym egzemplarzy klasy.
-//  wszystkie metody, które umieszczamy bezpśrednio w klasie (ponizej przykład addMember) są umeiszczane w prototypie klasy
+// Klasy nadrzędne (superclass), klasy pochodne, podrzędne (subclass-y) i dziedziczenie
 
-class Family {
-    constructor(name) {
-     this.name = name;
-     // this.addMember = function() {
-     //  console.log("wywołane z instancji");
-     // } //metoda byłaby umeiszczaona jako kopia w kazdej instancji klasy
+// klasa dziedziczy po innej klasie?
+// 1. zbudowanie prototypu klasy dziedziczącej w oparciu o klasę po której dziedziczy (można zapamietać w pewnym uproszczeniu, że powstaje łańcuch prototypów)
+// 2. wywywołanie konstruktora klasy(klas) nadrzędnej (nadrzędnych)
+
+// Słowo kluczowe extends - wskazanie klasy  po której nowa klasa powinna dziedziczyć.
+
+// Słowo kluczowe super (metoda) - służy do rozszerzania konstruktora nowej klasy o kontruktor klasy z której dziedziczy
+
+
+class Animal {
+    constructor(age, name) {
+     this.age = age;
+     this.name = name
     }
-    // ta metoda umieszczana jest w prototypie klasy, który współdzielą wszystkie instancje (obiekty powstałą w oparciu o klasę)
-    addMember() {
-     console.log("wywołane z prototypu");
+    breathe() {
+     console.log("Zwierze oddycha");
     }
-   
    }
    
-   // Tworzenie instancji na podstawie klasy odbywa się w taki sam sposób jak na podstawie funckji konstruktora 
-   const nowakowie = new Family("nowakowie")
+   const zwierze = new Animal(2, "Artek")
    
-   // Wywołanie metoda. Najpeirwsz szuka w instancji, potem w łacuhc prototypów (zaczyna od prototypu klasy)
-   nowakowie.addMember()
+   class Mammal extends Animal {
+    constructor(age, name, hairs) {
+     super(age, name);
+     this.hairs = hairs;
+     //wywołujemy konstruktor klasy nadrzędnej
    
-   // KLASA JEST ODPOWIEDNIKIEM KONSTRUKTORA, PONIŻEJ TO WIDAĆ DOSKONALE. UZYSKUJEMY TEN SAM EFEKT CO PRZY KLASIE
-   const Family2 = function(name) {
-    this.name = name;
-   }
-   const kowalscy = new Family2("kowalscy");
-   Family2.prototype.addMember = function() { }
-   
-   
-   
-   /* ----- Wyrażenie klasy ----- */
-   
-   // Wyrażenie funkcyjne 
-   const Person = function() { }
-   // Deklaracja klasy
-   function Person2() {
-   }
-   // Deklaracja klasy
-   class Person3 {
-   }
-   // Wyrażenie klasy
-   const Person4 = class {
-   }
-   
-   // PRZYKŁAD KLASY
-   
-   class Rodzina {
-    constructor(members, ...names) {
-     this.members = members;
-     this.names = names;
     }
-    addMember(newMember) {
-     this.names.push(newMember);
-     this.members++;
-     console.log(`Nowy członek rodziny: ${newMember}. Rodzina liczy teraz ${this.members} osób`);
+    drinkMilk() {
+     console.log("Ssaki piją mleko");
     }
    
-    //Metody statyczne deklarowane w klasie - Dodajemy prefiks static. Metody statyczne można wywołać z poziomu klasy (nie są dostępne z poziomu instancji)
-    static makeFamily(...memebers) {
-     return memebers;
+    speak() {
+     console.log('umm hum ummm')
     }
-   
    }
    
-   const stasiakowie = new Rodzina(3, 'Jan', 'Ewa', 'Adam')
+   const ssak = new Mammal(13, "Maks", 'ciemne')
+   ssak.breathe()
+   ssak.drinkMilk()
    
-   stasiakowie.addMember("Jaś");
+   class Human extends Mammal {
+    // jeśli nie damy constructora, to i tak będzie autoamtycnie wywołana metoda super. jesli damy w klasie (któa dziedziczy) constructor, to musimy w nim umieścić też metodę super.
+    constructor(age, name, hairs, language) {
+     super(age, name, hairs)
+     this.language = language;
+    }
    
-   const kwiatkowscy = new Rodzina(1, "Piotr");
+    speak() {
+     console.log('Człowiek mówi')
+    }
+   }
    
-   // Wywołanie metody statycznej (na konstruktorze, na klasie)
-   Rodzina.makeFamily('Maria', 'Filip')
+   const czlowiek = new Human(34, "Roman", "ciemne", "polski");
    
    
-   /* ----- Warto pamietać o klasach na tym etapie ----- */
    
-   // -- nie podlegają hoistingowi
-   // -- mniejsze mozliwości popełnienia błędu choćby nie da się wywołać bez new
-   // -- czytelniejsze
-   // -- uproszczenie w napisaniu wielu innych rzeczy jako np. dziedziczenie (extends). 
-   // -- co da się napisać za pomocą klasy, mozna też napisać w ES6, ale jest to bardziej (lub zdecydowanie bardziej) zawiłe. Tu jest proste.
+   
    
