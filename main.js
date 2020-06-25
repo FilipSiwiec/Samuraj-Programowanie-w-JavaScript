0a65e80ac4bb21110b1106aad9d2eac75465d854
Filip Siwiec <filipsiwiec@gmal.com>
2020-06-25 15:31:35 +0200

    section 15 - OOP - this - utrata wiazan

--- main.js
+++ main.js
@@ -1,172 +1,109 @@
-/* CO TO JEST THIS? */
-// -- this - słowo zawierajace odwołanie do obiektu. JavaScript przypisuje (zgodnie z regułami) do 'this' wiązanie z obiektem. 
-// -- O tym czym jest 'this' decyduje źródło (sposób) wywołania funkcji.
-// -- przypisanie do obiektu w this jest zawsze w chwili wywołania funkcji. Domyślnie ma charakter niejawny (a więc nie musimy wskazywać czym jest this, to stanie się automatycznie)
-// -- 'this' tworzy kontekst (obiekt) do którego możemy się odwołać w kodzie.
+'use strict'
+// Problem utraty wiązania this przez funkcje wywołaną w innej funkcji. Przykłady rozwiązania
+
+const szarik1 = {
+
+ children: ['fafik', 'żaba'],
+
+ showChildren: function() {
+  this.children.forEach(function(child, index) {
+   // na tym etapie wywowłanie tej funkcji anonimowej spowoduje, że nie będzie ona przejmowała this z funkcji wywołanej wszędzie lecz stworzy własne wiązanie domyślne z obiektem globalnym. W przypdku użycia 'use strict' będzie to wiązanie z undefined.
+   // console.log(this.children[index]); // odkomentowanie tej linijki powoduje błąd, bo window (czy undefined) nie ma własciwości children, która jest tablicą.
+   console.log(this); //window - wiązanie domyślne się pojawia
+  })
+ },
+}
+
+szarik1.showChildren()
+
+/* ROZWIĄZANIE */
+
+// 1. that = this
+
+const szarik2 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  const that = this;
+  // Zawartość this (a więc obiekt) przypisujemy do innej zmiennej (często nazywamy ją that, self). Teraz możemy odwołać się do tej zmiennej (zawierajacej "prawidłowe" wiązanie) w funkcji wewnątrz.
+  this.children.forEach(function(child, index) {
+   console.log(that.children[index]);
+   //zwórc uwagę, że w consoli napisaliśmy that.
+  })
+ },
+}
+
+szarik2.showChildren()
 
 
 
-/* DLACZEGO THIS JEST POTRZEBNE */
-// -- nie ma potrzeby używania precyzyjnych funkcji (osobnych dla każdego obiektu). Możliwość wielokrotnego użycia tej samej funkcji.
-// -- czystość w kodzie, łatwiejsza edycja kodu
+// 2. Bez funkcji (więc bez dodatkowego wywołania funkcji) - za pomocą pętli for of
 
-function showName() {
-    console.log(this.name)
-   }
-   
-   const karol = {
-    name: 'karol',
-    showName: showName
-   }
-   
-   const tatiana = {
-    name: 'tatiana',
-    showName: showName
-   }
-   
-   // const adam = {
-   //  name: 'adam',
-   //  showName: function() {
-   //   console.log(this.name)
-   //  }
-   // }
-   
-   // karol.showName()
-   // tatiana.showName()
-   // adam.showName()
-   
-   
-   /* KIEDY WIĄZANIE THIS POWSTAJE? */
-   // -- wiązanie this powstaje w chwili wykonywania metody i nie jest zależne wprost od miejsca deklaracji funkcji a od sposobu jej wywołania.
-   // -- w chwili wykonywania funkcji powstaje kontekst wykonawczy, którego częścią jest this.
-   
-   // function one() {
-   //  // 'use strict'
-   //  console.log(this);
-   //  this.name = "pierwsza";
-   //  return this.name;
-   // }
-   
-   // const object1 = {
-   //  two: one,
-   //  four: function() {
-   //   console.log(this);
-   //  }
-   // }
-   
-   // object1.two()
-   
-   // const three = object1.two
-   
-   // const five = object1.four;
-   // five()
-   
-   // three();
-   
-   
-   /* REASUMUJĄC NA TYM ETAPIE  */
-   // Źródłem powstania this jest wywołanie funkcji. Wiązanie powstaje w chwili wywołania funkcji/metody. Wiązanie this powstaje niezależnie czy w funkcji wykorzystujemy this (trochę tak jak przy wykryciu zdarzenia powstaje event tego zdarzenia. Też możemy, ale nie musimy go użyć)
-   
-   
-   
-   /* REGUŁY - DLACZEGO THIS JEST JAKIE JEST */
-   
-   // 1. wiązanie domyślne this to obiekt globalny, chyba że używamy 'use strict' (tryb ścisły), które na to nie pozwoli (będzie undefined)
-   
-   // const thisExample = function() {
-   //  // Tryb ścisły (stricte mode) - wartość domyślna this będzie undefined
-   //  // 'use strict';
-   //  console.log(this.example, this)
-   //  const inside = function() {
-   //   console.log(this.example, this)
-   //  }
-   //  inside()
-   // }
-   
-   // thisExample()
-   
-   
-   // 2. wiązanie niejawne - co jest obiektem kontekstu, co jest źródłem wywołania.
-   
-   
-   const user = {
-    age: 45,
-    showName() {
-     console.log(this.age);
-    },
-    showName2: function() {
-     console.log(this.age);
-    }
-   }
-   
-   // user.showName()
-   // user.showName2()
-   
-   const stefan = {
-    age: 20,
-    showName: user.showName
-   }
-   
-   // stefan.showName()
-   // Jeśli istnieje obiekt konteksu na którym wywołana jest metoda, to on zostanie przypisany to this.
-   
-   
-   
-   // 3. wiązanie jawne
-   // Mozemy powiedzieć funckji z czym ma powiązać this. Mówimy to przy wywołaniu funkcji. Metody call i apply
-   
-   const human = {
-    pesel: 20202020210,
-   }
-   
-   const showPesel = function() {
-    console.log(`Twój pesel to: ${this.pesel}`);
-   }
-   
-   // showPesel()
-   // showPesel.call(human);
-   
-   
-   const showUser = function(name, age) {
-    console.log(`użytkownik ${name}, w wieku ${age} ma pesel ${this.pesel}`);
-   }
-   
-   // showUser.call(human, "Tomasz", 42)
-   
-   // 4. wiązanie twarde
-   // bind - nowa funkcja ze stałym this
-   
-   const showPeselUser = showPesel.bind(human);
-   
-   // showPeselUser()
-   
-   // 5. wiązanie z operatorem new
-   
-   class User {
-    constructor(name) {
-     this.name = name
-    }
-   }
-   
-   const stasiek = new User('stasiek')
-   
-   
-   /* Arrow Function a this */
-   
-   // Arrow function nie tworzy podczas wywołania własnego wiązania this, a je dziedziczy, co czasami jest plusem a czasami minusem.
-   
-   // const cat = {
-   //  age: 45,
-   //  showAge() {
-   //   console.log(this.age, this);
-   //  },
-   //  showAge2: () => {
-   //   console.log(this.age, this);
-   //  }
-   // }
-   
-   // cat.showAge()
-   // cat.showAge2()
-   
-   
-   
+const szarik3 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  // Po prostu iterujemy po tablicy. Nie ma wywołania, nie powstaje więc nowe wiązanie this (pracujemy w tym samym kontekście, pętla nie tworzy nowego). 
+  for (const child of this.children) {
+   console.log(child);
+  }
+ },
+}
+
+szarik3.showChildren()
+
+
+// 3. arrow function - nie tworzy własnego wiązania this
+
+const szarik4 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  this.children.forEach((child, index) => {
+   console.log(this.children[index]);
+   // function(){} stworzy nowy this. ()=>{} nie tworzy nowego this, a dziedziczy je z aktualnego.
+  })
+ },
+}
+
+szarik4.showChildren()
+
+
+// 4. bind - trwałe wiązanie funckcji ze wskazanym this
+
+const szarik5 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  this.children.forEach(function(child, index) {
+   console.log(this.children[index]);
+  }.bind(this))
+  // metoda bind wywołana na funkcji zwraca inną funkcją, która jest taka sama, ale zawiera już wiązanie, ze wskazanym obiektem (to może być różny obiekt) W przykłądzie robimy wiązanie z this, które na tym etapie jest jeszcze obiektem na którym wywołujemy funkcję (dopiero wewnatrz funkcji callback straciłoby to wiązanie) 
+ },
+}
+
+szarik5.showChildren()
+
+
+// DODATKOWO PONAD FILM
+// 5 - wykorzystanie metody forEach do przekazania this
+// Metoda forEach oprócz callback, może przyjąć też this jako drugi argument (wtedy mimo utraty wiązania domyślnego mamy trwałe przypisanie this do callback, co też rozwiązuje nam problem).
+
+const szarik6 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  this.children.forEach(function(child, index) {
+   console.log(this.children[index]);
+  }, this) //w tym miejscu pojawił się drugi argument (pierwszym jest callback)
+ },
+}
+
+szarik6.showChildren()
+
+// Działa też ta metoda z arrow function
+const szarik7 = {
+ children: ['fafik', 'żaba'],
+ showChildren: function() {
+  this.children.forEach((child, index) => {
+   console.log(this.children[index]);
+  }, this)
+ },
+}
+
+szarik7.showChildren()
